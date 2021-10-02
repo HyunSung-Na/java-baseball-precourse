7ef61309f454593e590c4a1677d09d7ab86bfb3f
HyunSung-Na <oceana57@naver.com>
2021-10-02 12:21:03 +0900

    test: studyTest String charAt test add

--- src/test/java/studyTest/StringTest.java
+++ src/test/java/studyTest/StringTest.java
@@ -1,8 +1,10 @@
 package studyTest;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class StringTest {
 
@@ -33,5 +35,35 @@ public class StringTest {
 
         // when
         assertThat(substring).isEqualTo("1,2");
+    }
+
+    @DisplayName("특정 위치의 문자를 가져오는 테스트")
+    @Test
+    void charAt_특정위치의_문자를_가져온다() {
+        // given
+        String alpha = "abc";
+
+        // when
+        char charAtA = alpha.charAt(0);
+        char charAtB = alpha.charAt(1);
+        char charAtC = alpha.charAt(2);
+
+        // then
+        assertThat(charAtA).isEqualTo('a');
+        assertThat(charAtB).isEqualTo('b');
+        assertThat(charAtC).isEqualTo('c');
+    }
+
+    @DisplayName("위치를 벗어난 문자를 조회시 예외가 발생하는지 확인하는 테스트")
+    @Test
+    void charAt_문자의_범위를_벗어난_위치를_조회한다() {
+        // given
+        String alpha = "abc";
+
+        // then
+        assertThatThrownBy(() -> {
+            alpha.charAt(100);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 100");
     }
 }
