a5e3ce7d9913d251b087bf94eb4f61646a9f9489
HyunSung-Na <oceana57@naver.com>
2021-10-02 11:39:28 +0900

    test: studyTest String substring test add

--- src/test/java/studyTest/StringTest.java
+++ src/test/java/studyTest/StringTest.java
@@ -20,4 +20,18 @@ public class StringTest {
         assertThat(splitString).contains("2");
         assertThat(splitString).containsExactly("1", "2");
     }
+
+    @Test
+    void substring_문자열의_앞과_뒤를_분리되는지_확인() {
+        // given
+        String numbers = "(1,2)";
+        int start = 1;
+        int end = numbers.length() - 1;
+
+        // when
+        String substring = numbers.substring(start, end);
+
+        // when
+        assertThat(substring).isEqualTo("1,2");
+    }
 }
