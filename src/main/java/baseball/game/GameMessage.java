ef77d2c7f729913e2d4fb080ec8cfa06b335968a
HyunSung-Na <oceana57@naver.com>
2021-10-03 22:05:36 +0900

    feat: GameMessage class add

---
+++ src/main/java/baseball/game/GameMessage.java
@@ -0,0 +1,10 @@
+package baseball.game;
+
+public class GameMessage {
+    public final static String GAME_START_MESSAGE = "숫자를 입력해주세요 : ";
+    public final static String WRONG_USER_INPUT_ERROR_MESSAGE = "[ERROR] 잘못된 값을 입력했습니다. 다시 입력해주세요.";
+    public final static String GAME_SYSTEM_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public final static String GAME_END_MESSAGE = "게임이 종료되었습니다.";
+    public final static String GAME_SUCCESS_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public final static String NOT_MATCH_STRIKE_BALL_MESSAGE = "낫싱";
+}
