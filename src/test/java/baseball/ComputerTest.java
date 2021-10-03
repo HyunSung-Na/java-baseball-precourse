d33dc5369be3f902f2ea97a91945ce02501f4205
HyunSung-Na <oceana57@naver.com>
2021-10-03 11:29:14 +0900

    test: ComputerTest createFixedNumber test add

--- src/test/java/baseball/ComputerTest.java
+++ src/test/java/baseball/ComputerTest.java
@@ -6,6 +6,7 @@ import baseball.game.fixedNumberComputer;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -26,5 +27,26 @@ public class ComputerTest {
         assertThat(randomNumbers.get(0)).isNotEqualTo(randomNumbers.get(1));
         assertThat(randomNumbers.get(1)).isNotEqualTo(randomNumbers.get(2));
         assertThat(randomNumbers.get(0)).isNotEqualTo(randomNumbers.get(2));
+    }
+
+    @DisplayName("선택한 숫자를 생성한다")
+    @Test
+    void createFixedNumber() {
+        // given
+        List<Character> numbers = new ArrayList<>();
+        numbers.add('1');
+        numbers.add('2');
+        numbers.add('3');
+
+        Computer computer = new fixedNumberComputer(numbers);
+
+        // when
+        List<Character> fixedNumber = computer.getNumber();
+
+        // then
+        assertThat(fixedNumber.size()).isEqualTo(numbers.size());
+        assertThat(fixedNumber.get(0)).isEqualTo(numbers.get(0));
+        assertThat(fixedNumber.get(1)).isEqualTo(numbers.get(1));
+        assertThat(fixedNumber.get(2)).isEqualTo(numbers.get(2));
     }
 }
